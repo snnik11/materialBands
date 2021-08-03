d347f8adb071056eb941cc572b1ebc4f7026a9fa
CharlesSwei <sweicharles@gmail.com>
2021-08-03 23:31:58 +1000

    Make nav bar item distributed evenly

--- src/layouts/DocsLayout/components/Navb.js
+++ src/layouts/DocsLayout/components/Navb.js
@@ -4,41 +4,23 @@ import { Navbar, Nav } from 'react-bootstrap';
 //import logo from '../images/logo.png';
 
 const navlinkStyle = {
-  //paddingRight: '35px',
-  // paddingTop: '30px',
-  // paddingBottom: '20px',
+  textDecoration: 'none',
   fontWeight: 'bold',
-  fontSize: '30px',
-  color: '#3b4454',
-  //justifyContent: 'space-evenly',
-  //'black',
-  // 'white',
-  // textAlign: 'right',
+  fontSize: '26px',
+  color: '#3f51b5',
   backgroundColor: '#e8eaf6',
-  // marginRight: 'auto',
-  // marginLeft: 'auto',
-  paddingLeft: '10px',
-  //'black',
-  // ' #cbd3d8 ',
+  marginHorizontal: '10px',
+  paddingHorizontal: '25px',
 };
 
 const navbarStyle = {
-  paddingRight: '25px',
-  //paddingLeft: '1300px',
-  paddingTop: '10px',
-  paddingBottom: '20px',
   fontSize: '28px',
   color: 'black',
-
   display: 'flex',
-  //'white',
   textAlign: 'justify',
-  //'right',
+  alignItems: 'center',
+  justifyContent: 'space-evenly',
   backgroundColor: '#e8eaf6',
-  // 'black',
-  //' #cbd3d8 ',
-
-  justifyContent: 'right',
 };
 function Navb() {
   return (
@@ -81,80 +63,59 @@ function Navb() {
                 Separated link
               </NavDropdown.Item>
             </NavDropdown> */}
-          <span>
-            <img
-              className="bands-logo"
-              src="https://mybands.s3.ap-southeast-2.amazonaws.com/logosem2/bands.png"
-              //"https://mybands.s3.ap-southeast-2.amazonaws.com/logos/bands.png"
-              //"https://mybands.s3.ap-southeast-2.amazonaws.com/events/bandslogo.png"
-              alt="BANDS"
-              style={{
-                width: 135,
-                height: 120,
-                paddingTop: '10px',
-
-                paddingLeft: '20px',
-                fontFamily: 'Lato',
-              }}
-            ></img>
-          </span>
           <Nav className="mr-auto" style={navbarStyle}>
             <span>
-              {/* QUTBANDS */}
-              <Nav.Link
-                className="nav-links"
-                href="/"
+              <img
+                className="bands-logo"
+                src="https://mybands.s3.ap-southeast-2.amazonaws.com/logosem2/bands.png"
+                //"https://mybands.s3.ap-southeast-2.amazonaws.com/logos/bands.png"
+                //"https://mybands.s3.ap-southeast-2.amazonaws.com/events/bandslogo.png"
+                alt="BANDS"
                 style={{
-                  navlinkStyle,
-
-                  // paddingRight: '35px',
-                  // paddingTop: '10px',
-                  // paddingBottom: '20px',
-                  // paddingLeft: '10px',
-                  // fontSize: '30px',
-                  // fontWeight: 'bold',
-                  // color: '#3b4454',
-
-                  // 'black',
-                  //'white',
-                  // textAlign: 'right',
-                  // backgroundColor: '#e8eaf6',
-
-                  //  display: 'flex',
-                  //'black',
-                  //  ' #cbd3d8 ',
+                  width: 'auto',
+                  height: '5rem',
+                  padding: '10px 15px',
+                  fontFamily: 'Lato',
                 }}
-              >
+              ></img>
+            </span>
+
+            {/* QUTBANDS */}
+            <div style={{ navlinkStyle }}>
+              <Nav.Link className="nav-links" href="/">
                 Home
               </Nav.Link>
+            </div>
+            <div style={navlinkStyle}>
               <Nav.Link
-                className="nav-links"
+                // className="nav-links"
                 href="/about"
-                style={navlinkStyle}
               >
                 About Us
               </Nav.Link>
-              {/* <Nav.Link className = "nav-links"  href="/team">Team</Nav.Link> */}
+            </div>
+            {/* <Nav.Link className = "nav-links"  href="/team">Team</Nav.Link> */}
+            <div style={navlinkStyle}>
               <Nav.Link
-                className="nav-links"
+                // className="nav-links"
                 href="/blog-reach-view"
-                style={navlinkStyle}
               >
                 Events
               </Nav.Link>
-              {/* <Nav.Link className="nav-links" href="/IndustryPartners">
+            </div>
+            {/* <Nav.Link className="nav-links" href="/IndustryPartners">
               ind
-            </Nav.Link> */}
-              {/* <Nav.Link href="/faqs">FAQs</Nav.Link> */}
-              {/* <Nav.Link className = "nav-links" href="/blog">Blog</Nav.Link> */}
+              </Nav.Link> */}
+            {/* <Nav.Link href="/faqs">FAQs</Nav.Link> */}
+            {/* <Nav.Link className = "nav-links" href="/blog">Blog</Nav.Link> */}
+            <div style={navlinkStyle}>
               <Nav.Link
-                className="nav-links"
+                // className="nav-links"
                 href="/industry_partners"
-                style={navlinkStyle}
               >
                 Industry Partners
               </Nav.Link>
-            </span>
+            </div>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
